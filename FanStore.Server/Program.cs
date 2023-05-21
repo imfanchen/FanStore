6aca6939acbac83c1808f8b6ea2f80dcbd35cd25
Fan Chen <fan.chen@live.com>
2023-05-21 17:25:50 -0400

    Use a json logger

--- FanStore.Server/Program.cs
+++ FanStore.Server/Program.cs
@@ -8,6 +8,14 @@ builder.Services.AddRepositories(builder.Configuration);
 builder.Services.AddAuthentication().AddJwtBearer();
 builder.Services.AddClaimBasedAuthorization();
 
+builder.Logging.AddJsonConsole(options =>
+{
+    options.JsonWriterOptions = new()
+    {
+        Indented = true
+    };
+});
+
 WebApplication app = builder.Build();
 
 await app.Services.InitializeDatabase();
